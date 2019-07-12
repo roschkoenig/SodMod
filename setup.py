7856d9e0332b7b56cead58abd9f623a1b0c7e168
Richard Rosch <roschkoenig@gmail.com>
2019-07-13 01:00:59 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name          = 'sodmod',
+      version       = '0.1',
+      description   = 'Sodium Modelling for SCN1A',
+      url           = '',
+      author        = 'Richard Rosch',
+      author_email  = 'roschkoenig@gmail.com',
+      license       = 'MIT',
+      packages      = ['sodmod'],
+      zip_safe      = False
+      )
---
+++ sodmod/__init__.py
@@ -0,0 +1,5 @@
+from sodmod import calc
+from sodmod import cells
+from sodmod import chans
+from sodmod import incurr
+from sodmod import params
