fde243669ca88891ce6e938d3755c8802493af7e
Richard Rosch <roschkoenig@gmail.com>
2020-04-13 13:50:06 +0100

    Bug fix

--- sodmod/synapse.py
+++ sodmod/synapse.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def Tgaba(s,p):
     T = np.zeros(s['GABA'].shape)
-    T[np.where(s > 0)[0]] = p['GABAamp']
+    T[np.where(s['GABA'] > 0)[0]] = p['GABAamp']
 
 #-------------------------------------------------------------------------------
 # GABA mediated current
@@ -11,4 +11,3 @@ def GABA(Vm, p, r, s):
     igaba = r * p['gGABA']* (Vm - p['Egaba'])
     dr    = np.multiply(p['aGABA']*Tgaba(s,p),(1-r)) - r * p['bGABA']
     return (igaba, dr)
-    
