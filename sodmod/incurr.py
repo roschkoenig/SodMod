64df7de15fcc09c4e6a405933e0f0325a0c5ef83
Richard Rosch <roschkoenig@gmail.com>
2020-04-14 14:34:52 +0100

    Updated steepndess

--- sodmod/incurr.py
+++ sodmod/incurr.py
@@ -8,7 +8,7 @@ def I_step(t):
 def I_constant(t):  return 1.0
 
 def I_ramp(t):
-    steepness = 5
+    steepness = 10
     if 0.0 < t < 20.0:  return 0
     if 20.0 < t < 980.0:
         tctl = steepness*(t-20.0)/960.0
