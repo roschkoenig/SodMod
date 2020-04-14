e4abb7506fac2ef6b92f841584fe43c1b65aa153
Richard Rosch <roschkoenig@gmail.com>
2020-04-14 14:28:00 +0100

    bug fix

--- sodmod/incurr.py
+++ sodmod/incurr.py
@@ -9,7 +9,7 @@ def I_ramp(t):
     if 0.0 < t < 20.0:  return 0
     if 20.0 < t < 980.0:
         tctl = (t-20.0)/960.0
-        return (np.exp(tctl)-1) / (np.exp(1)-1))        
+        return (np.exp(tctl)-1) / (np.exp(1)-1)        
     return 0.0
 
 def Id(t, paradigm):
