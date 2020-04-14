14b3017fc4bad490689694647a20978c62fec54f
Richard Rosch <roschkoenig@gmail.com>
2020-04-14 13:50:13 +0100

    Incorporate Ramp

--- sodmod/incurr.py
+++ sodmod/incurr.py
@@ -5,6 +5,12 @@ def I_step(t):
 
 def I_constant(t):  return 1.0
 
+def I_ramp(t):
+    if 0.0 < t < 20.0:  return 0
+    if 20.0 < t < 980.0: return np.log(t-20.0)/960.0
+    return 0.0
+
 def Id(t, paradigm):
     if paradigm == 'step':  return I_step(t)
     if paradigm == 'constant': return I_constant(t)
+    if paradigm == 'ramp': return I_ramp(t)
