102b01ca0ebddde3e1e23fd4bb051ece5fcb1e47
Richard Rosch <roschkoenig@gmail.com>
2020-04-16 12:15:43 +0100

    Revised incurrents in line with master
    Mainly added the ramp
    not sure why it got lost in the first place

--- sodmod/incurr.py
+++ sodmod/incurr.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def I_step(t):
     if   0.0  < t < 20.0:    return 0
     elif 20.0 < t < 100.0:   return 1
@@ -5,6 +7,15 @@ def I_step(t):
 
 def I_constant(t):  return 1.0
 
+def I_ramp(t):
+    steepness = 10
+    if 0.0 < t < 20.0:  return 0
+    if 20.0 < t < 980.0:
+        tctl = steepness*(t-20.0)/960.0
+        return (np.exp(tctl)-1) / (np.exp(steepness)-1)
+    return 0.0
+
 def Id(t, paradigm):
     if paradigm == 'step':  return I_step(t)
     if paradigm == 'constant': return I_constant(t)
+    if paradigm == 'ramp': return I_ramp(t)
